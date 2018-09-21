b9f334090262c503954be629d3ef72a7623e6efe
Marcos da Cruz Sibilio Jr <marcos@marcos-Aspire-E1-571.lan>
2018-09-21 12:07:59 -0300

    Commit inicial

---
+++ base de dados/cadastros/conjunto_de_palavras.php
@@ -0,0 +1,285 @@
+<?php
+
+$conjunto = [
+	'imovel' => [
+		'casa', 'casas', 'alugar', 'aluguel', 'alugueis', 'imovel', 'imoveis', 'temporada', 'apartamento', 'apartamentos', 'terreno', 'terrenos', 'kitnet', 'ktnets', 'loja', 'lojas', 'corretora', 'corretor', 'corretoras', 'corretores', 'imobiliaria', 'imobiliarias'
+	],
+	'contabilidade' => [
+		'contabilidade', 'contabilidades', 'escritura', 'escrituras', 'escrituração', 'escrituracoes', 'serviço','servicos', 'livro','livros', 'caixa', 'caixas', 'funcionarios', 'funcionario', 'acordo', 'acordos',
+	],
+	'papelaria' => [
+		'caderno','cadernos', 'lapis', 'xerox',
+	],
+	'centro automotivo' => [
+		'suspensao', 'suspensoes', 'freio', 'freios','injecao',  'limpeza', 'bico', 'bicos', 'arrefecimento', 'troca', 'oleo', 'alinhamento','balanceamento', 'borracharia', 'radiador', 'mecanico', 'mecanica', 'direcao', 'amortecedores', 'amortecedor', 'centro', 'automotivo', 'roda', 'rodas', 'pneu', 'pneus'
+	],
+	'madeira' => [
+		'madeira', 'madeiras', 'aparelhada', 'aparelhadas', 'telhado', 'telhados', 'porta', 'portas', 'batente', 'batentes','guarnicao', 'guarnicoes', 'lambril', 'portao', 'portoes', 'escada', 'escadas'
+	],
+	'roupa' => [
+		'camisa', 'camisas', 'camiseta', 'camisetas', 'short', 'shorts', 'permudas', 'permuda', 'calcas', 'calca', 'jeans'
+	],
+	'academia' => [
+		'muculacao', 'fitness', 'execicio', 'exercicios','aerobica', 'aerobicas', 'aerobico', 'aerobicos', 'arte', 'marcial', 
+	],
+	'acougue' => [
+		'carne', 'carnes', 'frango', 'churrasco', 'acougue', 'acougues'
+	],
+	'agua' => [
+		'agua mineral', 'garrafao', 'agua', 'garrafoes'
+	],
+	'pescaria' => [
+		'anzol', 'linha', 'vara', 'molinete', 'anzois', 'linhas', 'varas',  'pescar', 'pesca', 'pescaria', 'molinetes', 
+	],
+	'andaime' => [
+		'andaime', 'andaimes'
+	],
+	'manutencao maquina' => [
+		'manutencao', 'maquina', 'rocadeira', 'vap', 'maquinas', 'monutencoes'
+	],
+	'auto escola' => [
+		'formacao',  'condutor', 'condutores', 'carro', 'moto', 'carta', 'habilitacao', 'carteira', 'motorista'
+	],
+	'auto eletrica' => [
+		'auto', 'eletrica', 'bateria', 'alternador', 'eletrico', 'baterias'
+	],
+	'auto pecas' => [
+		'auto', 'peca', 'pecas'
+	],
+	'som automotivo' => [
+		'som', 'automotivo', 'alarmes', 'alarme', 'trava', 'eletrica', 'vidro', 'vidros', 'eletrico', 'alto', 'falante', 'sonorizacao', 'automotiva', 'dvd', 'insul', 'film', 'insufilme', 'insulfilme', 'acessorio', 'acessorios'
+	],
+	'bar' => [
+		'bar', 'bares',
+	],
+	'tosa' => [
+		'banho', 'tosa', 'banhos',
+	],
+	'bercario' => [
+		'bercario', 'bercarios'
+	],
+	'borracharia' => [
+		'borracharia', 'borracharias',
+	],
+	'cabeleireiro' => [
+		'cabeleireiro', 'escova', 'tingimento', 'cabeleireiros', 'cabelo', 'corte'
+	],
+	'loja sapato' => [
+		'sapato', 'tamanco', 'salto', 'tenis', 'sapatos', 'tamancos', 'saltos',
+	],
+	'casa norte' => [
+		'carne', 'carnes', 'seca', 'farinha', 'mandioca', 'manteiga', 'ingrediente', 'ingredientes', 'feijoada', 'doce', 'doces', 'biscoito', 'biscoitos', 'bolacha', 'bolachas'
+	],
+	'seguranca' => [
+		'vigilancia', 'alarme', 'alarmes','seguranca', 'camera', 'cameras', 'monitoramento', 'ronda', 'cftv', 'c.f.t.v.', 'cerca',  'cercas','eletrica', 'eletricas'
+	],
+	'petshop' => [
+		'racao', 'cachorro', 'gato', 'semente', 'passarinho', 'racoes', 'cachorros', 'gatos', 'sementes', 'passarinhos', 'passaro', 'passaros', 'alimento', 'remedio', 'medicamento', 'animal', 
+	],
+	'loja celular' => [
+		'celular', 'bateria', 'carregador', 'manutencao', 'celulares', 'baterias', 'carregadores',
+	],
+	'cesta cafe' => [
+		'cesta', 'cafe', 'chocolate', 'romantica', 'namorado', 'casal', 'casamento','caneca', 'foto', 'relogio', 'guloseima', 'coracao', 'cestas', 'namorados', 'casais', 'casamentos', 'canecas', 'fotos', 'relogios', 'guloseimas', 'coracoes'
+	],
+	'chaveiro' => [
+		'chaveiro', 'chaveiros', 'abertura', 'portas', 'porta', 'fechadura', 'carro', 'carros', 'fechaduras'
+	],
+	'centro esportivo' => [
+		'quadra', 'futsal', 'esporte', 'salao', 'festa', 'escolinha', 'evento', 'quadras', 'esportes', 'saloes', 'festas', 'escolinhas', 'eventos',
+	],
+	'churrascaria' => [
+		'rodizio', 'self', 'service', 'marmitex', 'embalagem', 'viagem', 'embalagens', 'viagens'
+	],
+	'buffet' => [
+		'buffet', 'festa', 'material', 'decoracao', 'organizacao', 'realizacao' , 'buffets', 'festas', 'materiais', 'decoracoes', 'organizacoes', 'realizacoes', 'evento', 'eventos'
+	],
+	'farmacia' => [
+		'remedio', 'calmante', 'pomada', 'xarope', 'remedios', 'calmantes', 'pomadas', 'xaropes', 
+	],
+	'dentista' => [
+		'dentista', 'consultorio', 'aparelho', 'dentario', 'canal', 'tratamento', 'dentistas', 'consultorios', 'aparelhos', 'dentarios', 'canais', 'tratamentos',
+	],
+	'descartavel' => [
+		'descartavel', 'embalagem', 'artigo', 'festa', 'copo', 'descartavel', 'embalagens', 'artigos', 'festas', 'copos'
+	],
+	'serralheria' => [
+		'aluminio', 'box', 'janela', 'janelas', 'portão', 'portoes', 'automatizacao', 'porta', 'portas', 'grade', 'grades'
+	],
+	'vidracaria' => [
+		'vidro', 'vidros', 'box', 'temperado', 'janela', 'janelas', 'porta', 'portas', 'vitroux', 'vitro'
+	],
+	'ferragem' => [
+		'material', 'eletrico', 'hidraulica', 'parafuso', 'prego', 'materiais', 'eletricos', 'hidraulicas', 'parafusos', 'pregos'
+	],
+	'funilaria' => [
+		'reparo', 'funilaria', 'pintura', 'polimento', 'cristalizacao', 'recuperacao', 'reparos', 'pinturas', 'polimentos', 'cristalizacoes', 'recuperacoes',
+	],
+	'esfirraria' => [
+		'esfirra', 'fogazza', 'pastel', 'porcao', 'kit', 'salgado', 'esfirras', 'fogazzas', 'pasteis', 'porcoes', 'kits', 'salgados', 'esfiha', 'esfihas', 'comida', 'delivery', 'refeicao', 
+	],
+	'provedor' => [
+		'internet', 'fibra optica', 'provedor', 'provedores'
+	],
+	'informatica produto' => [
+		'teclado', 'mouse', 'roteador', 'cabo', 'rede', 'placa',  'mae', 'teclados', 'mouses', 'roteadores', 'cabos', 'redes', 'placas', 'informatica'
+	],
+	'informatica manutencao' => [
+		'manutencao', 'notebook', 'notebooks', 'computador', 'computadores', 'conserto', 
+	],
+	'impressora insumo' => [
+		'cartucho', 'cartuchos', 'impressora', 'toner', 'toners', 'multifuncional'
+	],
+	'impressora manutencao' => [
+		'manutencao', 'reparo', 'conserto', 'impressora', 'impressoras', 'multifuncional', 'multifuncionais', 'bulk', 'bulkink', 'ink'
+	],
+	'impressora marca' => [
+		'hp', 'canon', 'lexmark', 'xerox', 'brother', 'samsung', 'epson'
+	],
+	'lanchonete' => [
+		'lanche', 'hamburger', 'misto', 'quente', 'lanches', 'hamburgeres', 'comida',
+	],
+	'marcenaria' => [
+		'movel', 'sala', 'cozinha', 'banheiro', 'cama', 'planejada', 'closet', 'hack', 'divisoria', 'moveis', 'salas', 'cozinhas', 'banheiros', 'camas', 'planejadas', 'closets', 'hacks', 'divisorias'
+	],
+	'construcao' => [
+		'cimento', 'cal', 'areia', 'pedra', 'piso', 'pisos', 'material', 'construcao', 'bloco', 'blocos', 'laje', 'lajes', 'artefatos', 'artefato', 'eletrica', 'hidraulica', 'ferragens', 'ferragem', 'metais', 'louça', 'louças', 'sanitario', 'sanitarios', 'acabamento', 'acabamentos', 'porta', 'portas', 'janela', 'janelas', 'ferro', 'aluminio', 'tijolo', 'tijolos', 'madeira', 'madeiras', 'revestimento', 'revestimentos', 'porcelanato',
+	],
+	'entulho' => [
+		'cacamba', 'tira', 'entulho', 'limpa', 'obra', 'entulhos'
+	],
+	'toldo' => [
+		'toldo', 'toldos', 'cobertura', 'coberturas', 'policarbonato', 'luminosos', 'luminoso', 'letreiro', 'letreiros'
+	],
+	'calha' => [
+		'calhas', 'calha', 'rufo', 'rufos'
+	],
+	'comida japonesa' => [
+		'comida', 'japonesa', 'japones'
+	],
+	'dedetizadora' => [
+		'desinsetizacao', 'desratizacao', 'descupinizacao', 'limpeza', 'caixa', 'agua', 'rato', 'ratos', 'barata', 'baratas', 'caixas'
+	],
+	'grafica' => [
+		'cartao', 'cartoes', 'visita', 'carimbo', 'carimbos', 'envelope', 'envelopes', 'encadernacao', 'folhinha', 'folhinhas', 'calendario', 'calendarios', 'panfleto', 'panfletos'
+	],
+	'lava rapido' => [
+		'lavagem', 'carro', 'polimento', 'troca', 'oleo', 'enceramento', 'cristalizacao', 'filtro'
+	],
+	'marmoraria' => [
+		'pia', 'pias', 'piso', 'pisos','marmore', 'granito'
+	],
+	'moda infantil' => [
+		'calça', 'calças', 'camisa', 'camisas', 'camiseta', 'camisetas', 'bermuda', 'bermudas', 'vestido', 'vestidos', 'roupas', 'roupa'
+	],
+	'moda praia' => [
+		'maios', 'maio', 'biquini', 'biquinis', 'moda', 'praia', 'roupas', 'roupa', 'banho'
+	],
+	'moda intima' => [
+		'calcinha', 'loungerie', 'langeri', 'sutia', 'soutien', 'soutia', 'roupas', 'roupa'
+	],
+	'motos' => [
+		'alinhamento', 'chassi', 'macanica', 'mecanico', 'moto', 'eletrica', 'reforma', 'reformas', 'motos'
+	],
+	'motor eletrico' => [
+		'motor', 'motores', 'rebobinamento', 'rebobinar', 'enrolar', 'enrolamento', 'eletrico', 
+	],
+	'otica' => [
+		'oculos', 'lente', 'lentes', 'contato', 'bidocal', 'multifocal', 'otica', 'oticas', 'armacao', 'armacoes'
+	],
+	'moveis' => [
+		'movel', 'moveis', 'cama', 'camas', 'beliche', 'beliches', 'colchao', 'colchoes', 'escritorio', 'sofa', 'sofas', 'armario', 'armarios', 'cozinha', 'planejados', 'planejada'
+	],
+	'padaria' => [
+		'padaria', 'pao', 'paes', 'doces', 'doce', 'panificadora',
+	],
+	'docinho' => [
+		'brigadeiro', 'brigadeiros', 'beijinho', 'beijinhos', 'doces', 'doce', 'docinho', 'docinhos', 'festa', 'festas', 'aniversario',
+	],
+	'salgadinho' => [
+		'salgadinho', 'salgadinhos', 'salgado', 'salgados', 'coxinha', 'coxinhas', 'quibe', 'quibinho', 'festa', 'festas',
+	],
+	'bolo' => [
+		'aniversario', 'aniversarios', 'festa', 'festas', 'casamento', 'casamentos', 'bolo','bolos', 'confeitado', 'confeitados'
+	],
+	'pesqueiro' => [
+		'pescaria', 'pesca', 'pesqueiro', 'peixe', 'peixes'
+	],
+	'piscina' => [
+		'cloro', 'produto', 'produtos', 'piscina', 'piscinas',
+	],
+	'podologia' => [
+		'podologia'
+	],
+	'pizzaria' => [
+		'comida', 'pizza', 'pizzas', 'esfirra', 'esfiha', 'pizzaria'
+	],
+	'delivery' => [
+		'delivery', 'entrega', 'domicilio', 'casa', 'disk'
+	],
+	'posto' => [
+		'posto', 'gasolina'
+	],
+	'hotel' => [
+		'hotel', 'reserva', 'reservas', 'quarto', 'quartos', 'pousada', 'chales'
+	],
+	'material hospitalar' => [
+		'material', 'hospitalar', 'hospitalares', 'produtos', 'produto', 'ortopedico', 'ortopedicos', 'cirurgico', 'cirurgicos', 'aparelho', 'aparelhos', 'convalecenca', 'cadeira', 'rodas', 'bengala', 'begalas', 'seringa', 'seringas'
+	],
+	'comida quilo' => [
+		'comida', 'quilo', 'kilo', 'prato', 'feito', 'self', 'service', 'serv', 'serve', 'carte', 'ala', 'a la'
+	],
+	'material limpeza' => [
+		'vassoura', 'vassouras', 'rodo', 'rodos', 'esfregao', 'esfregoes', 'cestos', 'cesto', 'cestas', 'cesta', 'baldes', 'balde', 'lixeira', 'lixeiras', 'saco', 'sacos', 'lixo', 'candida', 'cloro', 'material', 'limpeza'
+	],
+	'emporio' => [
+		'emporio'
+	],
+	'quiosque praia' => [
+		'porcao', 'porcoes', 'peixe', 'peixes', 'pescada', 'camarao', 'cerveja', 'suco', 'sucos', 'quiosque', 'praia', 'kioski', 'batida', 'batidas', 'lanche', 'lanches', 'petisco', 'petiscos'
+	],
+	'restaurante' => [
+		'porcao', 'porcoes', 'prato', 'pratos', 'a la', 'carte', 'quilo', 'kilo', 'comida', 'refeicao'
+	],
+	'sorveteria' => [
+		'sorveteria', 'buffet', 'sorvete', 'quilo', 'kilo', 'picole', 'picoles'
+	],
+	'pastelaria' => [
+		'pastelaria', 'pastel', 'pasteis'
+	], 
+	'reciclagem' => [
+		'latas', 'aluminio', 'sucata', 'latinhas', 'descarte', 'eletronicos', 
+	],
+	'mercado' => [
+		'mercado', 'super', 'supermercado'
+	],
+	'tapecaria' => [
+		'tapecaria', 'automotiva', 'banco', 'bancos', 'couro', 'conserto', 'reparo', 'sofa', 'poltrona', 'residencial', 'nautica', 'sofas', 'poltronas', 'estofado', 'estofados'
+	],
+	'revestimento produto' => [
+		'revestimento', 'massa', 'corrida', 'revestimentos', 'acrilica', 'pvc', 'niveladora', 'textura', 'grafiato' 
+	],
+	'gesso' => [
+		'moldura', 'forro', 'divisoria', 'painel', 'nicho', 'gesso', 'revestimento', 'acabamento'
+	],
+	'veiculo comercializacao' => [
+		'veiculo', 'veiculos', 'comercializacao', 'compra', 'venda', 'troca', 'vende', 'financia', 'financiamento', 'consignacao'
+	],
+	'terraplanagem' => [
+		'terraplanagem', 'limpeza', 'terreno', 'nivelamento', 'areia', 'pedra', 'demolicao', 'remocao', 'escavacao', 'piscina', 'tanque', 'tanques', 'entulho'
+	],
+	'farmacia manipulacao' => [
+		'manipulacao', 'formula', 'formulas', 'floral', 'florais', 'bach', 'produto', 'produtos', 'natural', 'naturais', 'homeopatia'
+	],
+	'joia oficina' => [
+		'oficina', 'joia', 'joias',
+	],
+	'joia venda' => [
+		'joias', 'joia', 'avalia', 'compra', 'vende'
+	],
+	'financiamento veicular' => [
+		'parcela', 'parcelas', 'financiamento', 'carro', 'veiculo', 'veicular', 'quitacao', 'atrasada', 'atraso', 'compra', 'quitacao'
+	],
+];
+
+return $conjunto;
---
+++ readme.md
@@ -0,0 +1,6 @@
+<h1>Móstrme</h1>
+Móstrame é um sistema concebido para ser um guia local.<br/>
+É um sistema de busca simplicado que retorna somente o nome da empresa, o que a empresa trabalha,
+o endereço e o telefone. A idéia é básica de simplismente servir como um ponto de referência
+a ser utilizado em pequenas cidades onde muitos comerciantes tem resistência a fazer uma página
+na internet para divulgação da empresa.
